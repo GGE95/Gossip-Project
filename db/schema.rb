2de4d93f68fb1a30b5bd929cf989b388372fbb04
ghislain <genet.ghislain@gmail.com>
2023-02-10 09:56:03 +0100

    message_commit4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_221406) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_222519) do
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.string "zip_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
+User.destroy_all
 10.times do
-  user = User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, email: Faker::Internet.email, age: rand(1..100))
+  user = User.create!(first_name: Faker::Name.name, last_name: Faker::Name.name, description: Faker::ChuckNorris.fact,email: Faker::Internet.email, age: rand(1..100))
 end
+
+# 10.times do
+#   city = City.create!(name: Faker::WorldCup.city, zip_code: Faker::Alphanumeric.alphanumeric(number: 10))
+# end
